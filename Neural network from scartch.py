0c2389f15b0a62749f66d786652c9193553882bb
charaf hamidi <34777843+charaf19@users.noreply.github.com>
2022-12-03 12:05:30 +0100

    initial commit

--- Neural network from scartch.py
+++ Neural network from scartch.py
@@ -4,18 +4,14 @@ X = [[1,2,3,2.5],
      [2.0,5.0,-1.0,2.0],
      [-1.5,2.7,3.3,0.8]
      ]
-inputs= [0,2,-1,3.3,-2.7,1.1,2.2,-100 ]
-output = []
-#first way to append 0 or i to the list 
-for i in inputs:
-    if i > 0:
-        output.append(i)
-    elif i<=0:
-        output.append(0)
-print(output)
-#second way 
-output1=[]
-for i in inputs:
-    output1.append(max(0,i))
-
-print(output1)
+#inputs= [0,2,-1,3.3,-2.7,1.1,2.2,-100 ]
+#output = []
+class Layer_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights= 0.10*np.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights)+self.biases
+class Activation_ReLU:
+    def forward(self,inputs):
+        self.output = np.maximum(0,inputs)
